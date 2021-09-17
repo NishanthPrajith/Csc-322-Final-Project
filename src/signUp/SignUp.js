5940e158d714b5007a99550306c970c505f2e450
Haroon Syed <63178444+Hxsyed@users.noreply.github.com>
2021-09-16 23:35:41 -0400

    sign up page
    worked on the sign up page

--- src/signUp/SignUp.js
+++ src/signUp/SignUp.js
@@ -9,13 +9,25 @@ export default function SignUp() {
     async function register(event) {
       event.preventDefault();
       var x = document.getElementById("frm2");
-      var name = x.elements[0].value; // user name
-      var username = x.elements[1].value; // test this @citymail.cuny.edu
-      var pass = x.elements[2].value; // user password
+      var firstname = x.elements[0].value; // user first name
+      var lastname = x.elements[1].value; // user last name
+      var dob = x.elements[2].value; // user dob 
+      var ssn4 = x.elements[3].value; // user last 4 digits of ssn 
+      var username = x.elements[4].value; // test this @citymail.cuny.edu
+      var pass = x.elements[5].value; // user password
       try {
-        await signup(username, pass, name);
+        await signup(username, pass, firstname, lastname,dob, ssn4);
       } catch {
         document.getElementById('error').style.display = "block";
+      }
+    }
+
+    function reveal(){
+      var x = document.getElementById("showtext");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
       }
     }
 
@@ -25,12 +37,27 @@ export default function SignUp() {
           <div className ="form">
             <p className = "title"> Sign Up </p>
             <form className ="login-form" id = "frm2">
-              <input type="text" className = "One" name = "FullName" autoComplete = "off" placeholder="Full Name" required/>
-              <label className = "three">Full Name</label>
+             {/* First Name */}
+              <input type="text" className = "One" name = "fname" autoComplete = "off" placeholder="First Name" required/>
+              {/* <label className = "three">First Name</label> */}
+            {/* Last Name */}
+              <input type="text" className = "One" name = "lname" autoComplete = "off" placeholder="Last Name" required/>
+              {/* <label className = "three">Last Name</label> */}
+            {/* Date of Birth */}
+              <input type="password" className = "One" name = "dob" autoComplete = "off" id = "showtext"  maxlength = "10" placeholder="Date of Birth (MM-DD-YYYY)" required/>
+              {/* <label className = "three">Date of Birth</label> */}
+              <input type="checkbox" onclick={reveal}/>Display Date of Birth
+            {/* SSN */}
+              <input type="password" className = "One" name = "dob" autoComplete = "off" id = "showtext2" maxlength = "4" placeholder="Last 4 digits of Social Security or CUNY-assigned ID Number" required/>
+              {/* <label className = "three">Date of Birth</label> */}
+              <input type="checkbox" onclick={reveal}/>Display last 4 digits 
+            {/* Email */}
               <input type="text" className = "Two" name = "Email" autoComplete = "off" placeholder="Email" required/>
-              <label className = "one">Email</label>
-              <input type="password" placeholder="Password" autoComplete = "off" required/>
-              <label className = "two">Password</label>
+              {/* <label className = "one">Email</label> */}
+            {/* Password */}
+              <input type="password" placeholder="Password" autoComplete = "off" id = "showtext1" required/>
+              {/* <label className = "two">Password</label> */}
+              <input type="checkbox" onclick={reveal}/>Show Password
               <p id = "error" className = "error">Account creation failed. <br></br> Check if exists.</p>
               <button onClick = {register}>Register</button>
               <p className ="message">Already registered? <Link to="/SignIn">Sign In</Link></p>
