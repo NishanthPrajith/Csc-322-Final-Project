184227c7af2aecb7b2274bfebf876ee7c881ce7c
Haroon Syed <63178444+Hxsyed@users.noreply.github.com>
2021-11-01 11:46:07 -0400

    About Us page finished 75 %

--- src/aboutus/AboutUs.js
+++ src/aboutus/AboutUs.js
@@ -11,7 +11,7 @@ import React, { useState, useEffect } from 'react';
 export default function AboutUs() {
   const [students, setStudents] = useState([]);
   const [tclasses, setTclasses] = useState([]);
-  const [lcasses, setLclasses] = useState([]);
+  const [lclasses, setLclasses] = useState([]);
   const [loading, setLoading] = useState(false);
 
 
@@ -71,14 +71,41 @@ export default function AboutUs() {
 
   return (
     <div className = "main">
-
-      { students.map((student) => (
-        <div>
-          <p> { student.First } </p>
-          <p> { student.Last } </p>
-          <p> { student.GPA } </p>
-        </div>
-      ))}
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <span className="tstudents">
+        <table>
+          <tr><th>First Name</th><th>Last Name</th><th>GPA</th></tr>
+        { students.map((student) => (
+          <tr>
+            <td> { student.First } </td>
+            <td> { student.Last } </td>
+            <td> { student.GPA } </td>
+          </tr>
+        ))}
+        </table>
+      </span>
+      <span className="tclasses">
+        <table>
+          <tr><th>Course</th><th>Rating</th></tr>
+        { tclasses.map((tclass) => (
+          <tr>
+            <td> { tclass.Course } </td>
+            <td> { tclass.Rating } </td>
+          </tr>
+        ))}
+        </table>
+      </span>
+      <span>
+        <table>
+          <tr><th>Course</th><th>Rating</th></tr>
+        { lclasses.map((lclass) => (
+          <tr>
+            <td> { lclass.Class } </td>
+            <td> { lclass.Rating } </td>
+          </tr>
+        ))}
+        </table>
+      </span>
     </div>
     )
 }
