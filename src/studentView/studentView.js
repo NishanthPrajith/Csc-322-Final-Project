07d316e48941eca013c98a7add33323bbfbd4602
clall000 <57602597+clall000@users.noreply.github.com>
2021-11-04 20:51:23 -0400

    student view create

---
+++ src/studentView/studentView.js
@@ -0,0 +1,23 @@
+import './studentView.css'
+import { Link } from "react-router-dom";
+import { userData } from '../contexts/userProfile';
+import { useAuth } from "../contexts/Authcontext";
+import { useHistory } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
+import { auth } from '../firebase';
+import { db } from "../firebase.js";
+import { collection, doc, query, getDoc, getDocs, onSnapshot } from 'firebase/firestore';
+import { getDatabase, ref, set } from "firebase/database";
+
+export default function studentView() {
+    const [Student, Student] = useState('');
+    const [CurrentClasses, setCurrentClasses] = useState([]);
+    const [CurrentRoster, setCurrentRoster] = useState([]);
+    const [Loading, setLoading] = useState('false');
+
+    async function getCourses() {
+        var myUserId = firebase.auth().currentUser.uid;
+
+        
+    }
+}
